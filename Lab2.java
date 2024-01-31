99a0114375466795eaf7a0792b51dc8069e756ba
dina.quevedo <dequevedo@loyola.edu>
2024-01-31 13:59:44 -0500

    :)

--- Lab2.java
+++ Lab2.java
@@ -1,13 +1,13 @@
-// Programmers:  [your names here]
-// Course:  CS 212
-// Due Date:
-// Lab Assignment:
-// Problem Statement:
-// Data In:
-// Data Out:
-// Credits: [Is your code based on an example in the book, in class, or something else?
-//            Reminder: you should never take code from the Internet or another person
+//Programmers: Dina Quevedo + Thomas Richards
+//Course: CS212, Mr. John
+//Due Date: 1/31/23
+//Lab Assignment: 2
+//Problem Statement: Create a lottery ticket generator that outputs 6 random numbers per 10 lottery tickets
+//Data In: customer name
+//Data Out: 6 random numbers per 10 lottery tickets, a good luck message, and the estimate jackpot amount
+//Credits: Lab Prompt
 
+//import Random, Scanner, and Decimal Format Utilities
 import java.util.Random;
 import java.util.Scanner;
 import java.text.DecimalFormat;
@@ -23,9 +23,13 @@ class Lab2 {
         System.out.print("What's your name? ");
         String customerName = input.nextLine();
 
+// Create variable for calling on the random utility
         Random rand = new Random();
+
+// Create variable for calling on the Jackpot Dollar Sign format
         DecimalFormat jackpotFMT = new DecimalFormat("$,###");
 
+// BEGINNING OF THE OUTPUT TO USER
         System.out.println("Here are the tickets: ");
         int num = 0;
         for(int i = 0; i < 10; i++){
@@ -41,10 +45,14 @@ class Lab2 {
                 }
             System.out.println();
             }
+//  Strip and Split the Users Name
         String[] names = customerName.strip().split(" ");
         System.out.println("-----------------");
-        System.out.println("Good luck "+ names[0]);
+//  Treat the name as a list and only print the first name
+        System.out.println("Good luck "+ names[0]"!");
         System.out.println("Estimated Jackpot:");
+
+//  Call on the $ format to print out the prize money amount
         System.out.println(jackpotFMT.format(prize));
         System.out.println("-----------------");
         }
