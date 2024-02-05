a91822d4018af0cc192f27d86de07baf09a096c0
Trichards25 <131497949+Trichards25@users.noreply.github.com>
2024-02-05 11:42:46 -0500

    Updated with comments still need algorithm

--- Lab2.java
+++ Lab2.java
@@ -15,32 +15,35 @@ import java.text.DecimalFormat;
 class Lab2 {
     public static void main(String[] args) {
 
+        //makes a new scanner called input
         Scanner input = new Scanner(System.in);
+        //  Create variables to call on random utility and Decimal Format utility
+        Random rand = new Random();
+        DecimalFormat jackpotFMT = new DecimalFormat("$,###.##");
 
+        //prints the purpose of the program
         System.out.println("CS 212 - Lab 2");
         System.out.println("This program generates 10 lottery tickets.");
-        
+
+        //gets the users name
         System.out.print("What's your name? ");
         String customerName = input.nextLine();
 
         //strips the name of any whitespace then splits the name by space in the middle
         String[] names = customerName.strip().split(" ");
 
-        //  Create variables to call on random utility and Decimal Format utility
-        Random rand = new Random();
-        DecimalFormat jackpotFMT = new DecimalFormat("$,###");
-
-        DecimalFormat betFMT = new DecimalFormat("$,###.##");
+        //gets input for initial bet
         System.out.print("How much do you want to play? ");
         double prize = input.nextDouble();
 
+        //outputs the persons name and how much they want to play
+        System.out.println("Great! "+ names[0] + " wants to play "+ jackpotFMT.format(prize));
 
-        System.out.println("Great! "+ names[0] + " wants to play "+ betFMT.format(prize));
-
+        //list of winning numbers from README
         int[] winningNums = { 3, 5, 16, 58, 59, 11};
 
 
-
+        //message to lt user know about to print the tickets
         System.out.println("Here are the tickets: ");
         //makes a new int called num
         int num = 0;
@@ -50,7 +53,9 @@ class Lab2 {
             for(int y = 0; y < 6; y++){
                 //generates a random int between 0 and 99
                 num = rand.nextInt(0,99);
+                //loops through each winning number
                 for (int j = 0; j < winningNums.length; j++){
+                    //compares the random to the winning numbers, and if it is, multiplies the prize by a power of 1.75
                     if (num == winningNums[j]){
                         prize = Math.pow(prize,1.75);
                     }
@@ -72,10 +77,11 @@ class Lab2 {
             }
         //outputs a line
         System.out.println("-----------------");
+        //outputs a goodluck message
         System.out.println("Good luck "+ names[0]);
         //outputs a line
         System.out.println("-----------------");
-        //outputs the estimated jackpot, and formats it so there is a $ in front and commas every 3 numbers
+        //outputs the winnings, and formats it so there is a $ in front and commas every 3 numbers
         System.out.println("Your winnings are :");
         System.out.println(jackpotFMT.format(prize));
         //outputs a line
